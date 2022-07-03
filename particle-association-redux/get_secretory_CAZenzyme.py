b52e7770848787e0e28afd66e46e97d0f34996fe
Juntao Zhong (Jimmy) <JuntaoZhong@users.noreply.github.com>
2022-07-02 20:53:24 -0400

    particle-association calibrate

--- particle-association-redux/get_secretory_CAZenzyme.py
+++ particle-association-redux/get_secretory_CAZenzyme.py
@@ -1,4 +1,8 @@
 import csv
 import glob
 
-all_CAZ = "../diamond_only_dbcan_overview.txt"
+all_CAZ = "../../diamond_only_dbcan_overview.txt"
+secretory_CAZ = "./secretory_CAZenzyme.txt"
+secretory_pep = "./secretory_peptidase"
+
+all_CAZ = list(csv.reader(open(all_CAZ), delimiter='\t'))
