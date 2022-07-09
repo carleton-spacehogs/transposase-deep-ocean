e804b44f6cdcaffd40bbe954fb8c79b49c86995c
JuntaoZhong <zhongj2@carleton.edu>
2022-07-09 07:23:43 -0500

    first batch ERR samples

--- identify_eukaryotes/MAGs_find_euk_contigs.sh
+++ identify_eukaryotes/MAGs_find_euk_contigs.sh
@@ -6,9 +6,11 @@ source activate tiara_euk # base environment has diamond
 # to run: ./MAGs_find_euk_contigs.sh
 
 # for ocean in SP SAT NAT deep; do
-for ocean in ARS CPC IN NP RS EAC MED; do
+# ARS  CPC  deep  EAC  IN  MED  NP  RS
+for ocean in deep; do
 	echo "currently working on $ocean ..." > logs/${ocean}.log
-	bins=$(ls ../../bins/${ocean}/TOBG_${ocean}*.fna)
+	# bins=$(ls ../../bins/${ocean}/TOBG_${ocean}*.fna)
+	bins=$(ls ../../bins/${ocean}/mp-deep_mag-*.fasta)
 	for MAG in $bins; do
 		MAG_name=$(echo $MAG | awk -F"/" '{print $5}')
 		outf="../../bins/${ocean}/${MAG_name%%.fna}/tiara_euk.txt"
