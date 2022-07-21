ddee1080cba8681605e2c49e8687be4227630cb9
JuntaoZhong <zhongj2@carleton.edu>
2022-07-21 12:45:29 -0500

    MAG pnps redux

--- MAG_pnps_redux/MAG_scg_individual_defense_pnps.py
+++ MAG_pnps_redux/MAG_scg_individual_defense_pnps.py
@@ -2,13 +2,13 @@ import sys
 import pandas as pd
 from MAG_singleCopyGene_defense_pnps_ratio import ocean_depths, merge_COG_category, get_all_pnps, get_binning_info
 
-def per_defense_ORFs_per_sample_pnps(ocean, root, depths):
+def per_defense_ORFs_per_sample_pnps(ocean, root, depths, avail_MAG):
 	bin_info = get_binning_info(root, ocean)
 	all_pnps = get_all_pnps(depths, root, ocean)
 	all_pnps = merge_COG_category(all_pnps, root, ocean)
 	defense_pnps = all_pnps[all_pnps['category_accession'].str.contains("V", na=False)]
 	defense_pnps = defense_pnps.merge(bin_info, on = "gene_callers_id")
-	defense_pnps = defense_pnps.merge(avail_MAG, how = "left", on = ["bin", "sample_id"]).reindex()
+	defense_pnps = defense_pnps.merge(avail_MAG, how = "inner", on = ["bin", "sample_id"]).reindex()
 	return defense_pnps
 
 o_depths = ocean_depths()
@@ -21,14 +21,14 @@ integrons = pd.read_csv("anvi_integrons_all_oceans.csv").astype(str)
 individual_defense_pnps = pd.DataFrame()
 for ocean, depths in o_depths.items():
 	print(ocean)
-	ocean_defense = per_defense_ORFs_per_sample_pnps(ocean, root, depths)
+	ocean_defense = per_defense_ORFs_per_sample_pnps(ocean, root, depths, avail_MAG)
 	ocean_defense["ocean"] = ocean
 	individual_defense_pnps = individual_defense_pnps.append(ocean_defense)
 
 defense_integron = individual_defense_pnps.merge(integrons, on=["ocean", "gene_callers_id"], how = "left")
 
 concise = defense_integron[["gene_callers_id", "pnps", "ocean", "depth", "bin", "defense_count",
-"sample_id", "signal_CAZyme_abun","scg_pnps_median",'integron']]
+"sample_id", "signal_CAZyme_abun","scg_pnps_median","scg_count",'integron']]
 
 concise.to_csv(path_or_buf=f'individual_defense_pnps.csv', sep=',', index=False)
 
