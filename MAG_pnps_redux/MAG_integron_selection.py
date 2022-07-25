fe1d31cdc56b9e4635929926bdc4668a386226c9
JuntaoZhong <zhongj2@carleton.edu>
2022-07-24 23:31:09 -0500

    MAG pnps redux

--- MAG_pnps_redux/MAG_integron_selection.py
+++ MAG_pnps_redux/MAG_integron_selection.py
@@ -1,5 +1,8 @@
 import pandas as pd
-from MAG_singleCopyGene_defense_pnps_ratio import ocean_depths, MAG_base_pnps, get_all_pnps
+import csv
+from MAG_singleCopyGene_defense_pnps_ratio import ocean_depths, MAG_base_pnps, get_all_pnps, get_binning_info
+
+deep_oceans = ["IN","NAT","SAT","NP","SP"]
 
 def within(start1, start2, end1, end2):
 	if abs(int(start1)-int(start2)) < 100 and abs(int(end1)-int(end2)) < 100:
@@ -17,14 +20,11 @@ def determine_if_integron(integron_dict, MAG_contig, MAG_start, MAG_end):
 				return g_type
 	return "non_integron"
 
-def get_integron_dict(integron_rt, ocean):
-	# int_f =f"{integron_rt}/{ocean}.integrons"
+def integron_dict_helper(integron_rt, ocean, integron_dict):
 	int_f =f"{integron_rt}/{ocean}/all_bins_db/Results_Integron_Finder_{ocean}_all_bins/all_integron.txt"
 	intgron_f = pd.read_csv(int_f,comment="#",sep="\t")
-	print(intgron_f)
 	integrons = intgron_f[["ID_replicon","pos_beg","pos_end","type"]].dropna()
 	integrons_2D = integrons.values.tolist()
-	integron_dict = {}
 	for l in integrons_2D:
 		contig, start, stop, gene_type = l
 		if start >= stop: 
@@ -35,20 +35,45 @@ def get_integron_dict(integron_rt, ocean):
 			integron_dict[str(contig)] = [[int(start), int(stop), str(gene_type)]]
 	return integron_dict
 
+def get_integron_dict(integron_rt, ocean):
+	# int_f =f"{integron_rt}/{ocean}.integrons"
+	ocean_integrons = {}
+	ocean_integrons = integron_dict_helper(integron_rt, ocean, ocean_integrons)
+	if ocean in deep_oceans:
+		ocean_integrons = integron_dict_helper(integron_rt, "deep", ocean_integrons)
+	return ocean_integrons
+
+def full_binning_info_helper(fname, source):
+	tmp = list(csv.reader(open(fname), delimiter="\t"))[1:]
+	bin_info = [l[:4] + l[1].split('_', 1) for l in tmp]
+	bin_info = pd.DataFrame(bin_info)
+	bin_info.columns = ["gene_callers_id","contig","start","stop","bin","bin_contig"]
+	bin_info['source'] = source
+	return bin_info
+
+def get_full_binning_info(root, ocean):
+	tail="all_bins_db/gene_callers_id-contig_full.txt"
+	bin_info = full_binning_info_helper(f"{root}/{ocean}/{tail}", 'tara')
+	if ocean in deep_oceans:
+		deep_bin = full_binning_info_helper(f"{root}/{ocean}/{tail}", 'malaspina')
+		bin_info = bin_info.append(deep_bin)
+	return bin_info
+
 def merge_anvi_gene_call(MAG_rt, ocean, integron_dict):
-	MAG_f = f"{MAG_rt}/{ocean}/all_bins_db/gene_callers_id-contig_full.txt"
-	MAGs = pd.read_csv(MAG_f, sep="\t")[["gene_callers_id","contig","start","stop"]]
-	MAG_2D = MAGs.values.tolist()
-	int_cols = ["gene_callers_id", "contig", "start", "stop", "integron", "ocean"]
+	MAG = get_full_binning_info(MAG_rt, ocean)
+	MAG_2D = MAG.values.tolist()
+	int_cols = list(MAG.columns) + ["integron", "ocean"]
 	MAG_integrons = []
 	for l in MAG_2D:
-		gene_callers_id,contig,start,stop = l
-		if start >= stop: 
-			exit(f"error while parsing: {MAG_f} --- start later than stop")
+		contig, start, stop = l[1:4]
+		start, stop = int(start), int(stop)
+		if start > stop:
+			print(f"{contig}, {start}, {stop}")
+			print(f"error while parsing: {ocean} --- start later than stop")
 		else:
 			is_integron = determine_if_integron(integron_dict, contig, start, stop)
 			if not is_integron == "non_integron":
-				MAG_integrons.append([gene_callers_id, contig, start, stop, is_integron, ocean])
+				MAG_integrons.append(l + [is_integron, ocean])
 	MAG_integrons_df = pd.DataFrame(MAG_integrons, columns=int_cols)
 	return MAG_integrons_df
 
@@ -59,64 +84,63 @@ def merge_pnps_all(df, o_depth, MAG_rt):
 		ocean_pnps["ocean"] = ocean
 		all_pnps = all_pnps.append(ocean_pnps)
 	merged_integrons_str = df.astype(str)
-	MAG_int_pnps = merged_integrons_str.merge(all_pnps, on=["gene_callers_id","ocean"])
+	MAG_int_pnps = merged_integrons_str.merge(all_pnps, on=["gene_callers_id","ocean","source"])
 	return MAG_int_pnps
+
+def COG_cate_helper(MAG_rt, ocean):
+	tail="all_bins_db/anvi_genes_COG_categories.tsv"
+	COG_cate_df = pd.read_csv(f"{MAG_rt}/{ocean}/{tail}", sep = "\t")
+	COG_cate_df['source'] = 'tara'
+	if ocean in deep_oceans:
+		deep_COG_cate = pd.read_csv(f"{MAG_rt}/deep/{tail}", sep = "\t")
+		deep_COG_cate["source"] = "malaspina"
+		COG_cate_df = COG_cate_df.append(deep_COG_cate)
+	out = []
+	for l in COG_cate_df.values.tolist():
+		COG_category_pos = 2
+		COG_cate = l[COG_category_pos]
+		if "!!!" in COG_cate:
+			all_COG_cates = COG_cate.split("!!!")
+			for COG in all_COG_cates:
+				out.append( l[:COG_category_pos] + [COG] + l[COG_category_pos+1:] )
+		else:
+			out.append(l)
+	COG_out = pd.DataFrame(out)
+	print(COG_out)
+	print(COG_cate_df)
+	COG_out.columns = list(COG_cate_df.columns)
+	COG_out["ocean"] = ocean
+	return COG_out.astype(str)
 
 def merge_COG_cate(df, o_depth, MAG_rt):
 	all_COG_cate = pd.DataFrame()
 	for ocean, depths in o_depth.items():
-		COG_f=f"{MAG_rt}/{ocean}/all_bins_db/anvi_genes_COG_categories.tsv"
-		COG_cate = pd.read_csv(COG_f, sep = "\t").astype(str)
-		COG_cate["ocean"] = ocean
+		COG_cate = COG_cate_helper(MAG_rt, ocean)
 		all_COG_cate = all_COG_cate.append(COG_cate)
-	df = df.merge(all_COG_cate, on=["gene_callers_id","ocean"])
+	df = df.merge(all_COG_cate, on=["gene_callers_id","ocean","source"], how = "left")
 	return df
 
 # main():
 o_depth = ocean_depths()
 MAG_rt="/researchdrive/zhongj2/MAG_pnps_redux"
-integron_rt="/researchdrive/zhongj2/integron_finder_tara_contig"
+# integron_rt="/researchdrive/zhongj2/integron_finder_tara_contig"
 
 merged_integrons = pd.DataFrame()
 for ocean in o_depth.keys():
 	print(ocean)
-	# integron_dict = get_integron_dict(integron_rt, ocean)
 	integron_dict = get_integron_dict(MAG_rt, ocean)
 	ocean_integrons = merge_anvi_gene_call(MAG_rt, ocean, integron_dict)
 	merged_integrons = merged_integrons.append(ocean_integrons)
-	print(ocean_integrons)
 
 merged_integrons.to_csv(path_or_buf=f'anvi_integrons_all_oceans.csv', sep=',', index=False)
-MAG_int_pnps = merge_pnps_all(merged_integrons, o_depth, MAG_rt)
-MAG_int_pnps = merge_COG_cate(MAG_int_pnps, o_depth, MAG_rt)
+tmp = merge_pnps_all(merged_integrons, o_depth, MAG_rt)
+MAG_int_pnps = merge_COG_cate(tmp, o_depth, MAG_rt)
+MAG_int_pnps.to_csv(path_or_buf=f'MAG_integron_pnps_all.csv', sep=',', index=False)
 
 bin_info = pd.read_csv("signalCAZyme-abun-vs-genes-pnps.csv").astype(str)
-int_pnps_col = list(MAG_int_pnps.columns)
-int_pnps_col.insert(1, "bin")
+MAG_int_pnps2 = MAG_int_pnps.merge(bin_info, on=["bin","sample_id"])
 
-MAG_int_2D = MAG_int_pnps.values.tolist()
-splited_init = [[l[0]] + l[1].split("_", 1) + l[2:] for l in MAG_int_2D]
-
-MAG_int_pnps2 = pd.DataFrame(splited_init)
-MAG_int_pnps2.columns = int_pnps_col
-
-MAG_int_pnps2 = MAG_int_pnps2.merge(bin_info, on=["bin","sample_id"])
-
-out = []
-for l in MAG_int_pnps2.values.tolist():
-	COG_category_pos = 11
-	COG_cate = l[COG_category_pos]
-	if "!!!" in COG_cate:
-		all_COG_cates = COG_cate.split("!!!")
-		for COG in all_COG_cates:
-			out.append( l[:COG_category_pos] + [COG] + l[COG_category_pos+1:] )
-	else:
-		out.append(l)
-
-MAG_int_pnps3 = pd.DataFrame(out)
-MAG_int_pnps3.columns = list(MAG_int_pnps2.columns)
-
-MAG_int_pnps3.to_csv(path_or_buf=f'MAG_integron_pnps.csv', sep=',', index=False)
+MAG_int_pnps2.to_csv(path_or_buf=f'MAG_integron_pnps_with_high_cov_bins.csv', sep=',', index=False)
 
 # if __name__ == "__main__":
 # 	main()
