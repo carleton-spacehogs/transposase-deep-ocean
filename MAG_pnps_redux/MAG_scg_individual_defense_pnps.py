20bf2374b38905c3bef6926b28224e2f91e43b71
JuntaoZhong <zhongj2@carleton.edu>
2022-08-13 22:14:18 -0500

    MAG pnps redux

--- MAG_pnps_redux/MAG_scg_individual_defense_pnps.py
+++ MAG_pnps_redux/MAG_scg_individual_defense_pnps.py
@@ -1,6 +1,6 @@
 import sys
 import pandas as pd
-from MAG_singleCopyGene_defense_pnps_ratio import merge_COG_category, get_binning_info
+from MAG_singleCopyGene_defense_pnps_ratio import merge_COG_category
 from utils import get_bin_helper, read_pnps, MAG_db_fun, has_deep, data_root, merge_blastp_f
 
 def get_all_pnps_v2(depths, root, ocean):
@@ -18,15 +18,17 @@ def get_all_pnps_v2(depths, root, ocean):
 	pnps_all_sam.sort_values(by=['gene_callers_id'])
 	return pnps_all_sam
 
-def per_defense_ORFs_per_sample_pnps(ocean, root, depths, MAG_db):
+# V: defense mechanism; T: signal transduction
+def per_category_ORFs_per_sample_pnps(ocean, root, depths, MAG_db, category):
 	bin_info = get_bin_helper(f"{root}/{ocean}/all_bins_db/gene_callers_id-contig.txt")
 	all_pnps = get_all_pnps_v2(depths, root, ocean)
 	all_pnps = merge_COG_category(all_pnps, root, ocean)
-	defense_pnps = all_pnps[all_pnps['category_accession'].str.contains("V", na=False)]
+	defense_pnps = all_pnps[all_pnps['category_accession'].str.contains(category, na=False)]
 	defense_pnps = defense_pnps.merge(bin_info, on="gene_callers_id")
 	defense_pnps = defense_pnps.merge(MAG_db, how = "inner", on = ["bin", "sample_id", "depth"]).reindex()
 	return defense_pnps
 
+# gene: toxin, transposase
 def per_gene_ORFs_per_sample_pnps(ocean, root, depths, MAG_db, gene):
 	bin_info = get_bin_helper(f"{root}/{ocean}/all_bins_db/gene_callers_id-contig.txt")
 	all_pnps = get_all_pnps_v2(depths, root, ocean)
@@ -53,11 +55,21 @@ all_MAG_pnps = pd.read_csv("MAG_info_db/MAG_all.csv")
 individual_defense_pnps = pd.DataFrame()
 for ocean, depths in o_depths.items():
 	print(ocean)
-	ocean_defense = per_defense_ORFs_per_sample_pnps(ocean, data_root, depths, all_MAG_pnps)
+	ocean_defense = per_category_ORFs_per_sample_pnps(ocean, data_root, depths, all_MAG_pnps, "V")
 	individual_defense_pnps = individual_defense_pnps.append(ocean_defense)
 
 defense_integron = add_integrons(id_col, individual_defense_pnps)
 defense_integron[out_col].to_csv(path_or_buf=f'individual_defense_pnps.csv', sep=',', index=False)
+
+# for the signal tranductions
+individual_signal_pnps = pd.DataFrame()
+for ocean, depths in o_depths.items():
+	print(ocean)
+	ocean_signal = per_category_ORFs_per_sample_pnps(ocean, data_root, depths, all_MAG_pnps, "T")
+	individual_signal_pnps = individual_signal_pnps.append(ocean_signal)
+
+signal_integron = add_integrons(id_col, individual_signal_pnps)
+signal_integron[out_col].to_csv(path_or_buf=f'individual_signal_pnps.csv', sep=',', index=False)
 
 # for transposase
 individual_trans_pnps = pd.DataFrame()
