23f619aaa46db23730206f0bfb0f0cc7e1f478aa
JuntaoZhong <zhongj2@carleton.edu>
2022-09-16 20:24:28 -0500

    Eukdetect map all samples

--- OM-RGC-and-abundance/cal_cov_mala_v2.py
+++ OM-RGC-and-abundance/cal_cov_mala_v2.py
@@ -30,31 +30,32 @@ def cal_sum_merge_md(gene_rows, md, gene):
 	md[gene] = md[gene]*150/1000000000/md.Sequencing_depth_Gbp
 	return md
 
-reference = pd.read_csv(f"{base1}/deep-ref-abundance.csv")
-reference = reference.rename(columns = {'id':'gene_ID'})
+def main():
+	reference = pd.read_csv(f"{base1}/deep-ref-abundance.csv")
+	reference = reference.rename(columns = {'id':'gene_ID'})
 
-biofilm_id = read_blastp(f"{base1}/diamond-blastp/biofilm_diamond.blastp", 1)
-trans_id = read_blastp(f"{base1}/diamond-blastp/transposase_diamond.blastp", 1)
-CAZ_id = pd.read_csv("../particle_association_redux/deep_CAZyme_ID.txt", names=["gene_ID"], header=None)
-pep_id = pd.read_csv("../particle_association_redux/deep_peptidase_ID.txt", names=["gene_ID"], header=None)
-sect_CAZ_id = pd.read_csv("../particle_association_redux/deep_secretory_CAZyme.txt")
-sect_pep_id = pd.read_csv("../particle_association_redux/deep_secretory_peptidase.txt")
+	biofilm_id = read_blastp(f"{base1}/diamond-blastp/biofilm_diamond.blastp", 1)
+	trans_id = read_blastp(f"{base1}/diamond-blastp/transposase_diamond.blastp", 1)
+	CAZ_id = pd.read_csv("../particle_association_redux/deep_CAZyme_ID.txt", names=["gene_ID"], header=None)
+	pep_id = pd.read_csv("../particle_association_redux/deep_peptidase_ID.txt", names=["gene_ID"], header=None)
+	sect_CAZ_id = pd.read_csv("../particle_association_redux/deep_secretory_CAZyme.txt")
+	sect_pep_id = pd.read_csv("../particle_association_redux/deep_secretory_peptidase.txt")
 
-md_cols = "sample,station,lower_filter_size,Ocean_DNA,Ocean,Depth,Longitude,Latitude,Salinity_PSU,Temperature,Oxygen_DNA,WATERMASS,Basin,Sequencing_depth_Gbp".split(",")
-metadata = pd.read_csv(f"{base}/deep_metagenome_transposase_BLAST/sample_metadata.tsv",
-					sep = "\t", names = md_cols, header = 0)
+	md_cols = "sample,station,lower_filter_size,Ocean_DNA,Ocean,Depth,Longitude,Latitude,Salinity_PSU,Temperature,Oxygen_DNA,WATERMASS,Basin,Sequencing_depth_Gbp".split(",")
+	metadata = pd.read_csv(f"{base}/deep_metagenome_transposase_BLAST/sample_metadata.tsv",
+						sep = "\t", names = md_cols, header = 0)
 
-single_genes_id = [biofilm_id, trans_id, CAZ_id, pep_id, sect_CAZ_id, sect_pep_id]
-for i in range(len(tara.single_genes)):
-	gene_rows = single_genes_id[i].merge(reference, on="gene_ID")
-	metadata = cal_sum_merge_md(gene_rows, metadata, tara.single_genes[i])
+	single_genes_id = [biofilm_id, trans_id, CAZ_id, pep_id, sect_CAZ_id, sect_pep_id]
+	for i in range(len(tara.single_genes)):
+		gene_rows = single_genes_id[i].merge(reference, on="gene_ID")
+		metadata = cal_sum_merge_md(gene_rows, metadata, tara.single_genes[i])
 
-for cog_category in tara.COG_list:
-	COG_cat_id = get_gene_specific_ref_through_COG(cog_category)
-	gene_rows = reference[reference.COG.str.contains(COG_cat_id)]
-	metadata = cal_sum_merge_md(gene_rows, metadata, cog_category)
+	for cog_category in tara.COG_list:
+		COG_cat_id = get_gene_specific_ref_through_COG(cog_category)
+		gene_rows = reference[reference.COG.str.contains(COG_cat_id)]
+		metadata = cal_sum_merge_md(gene_rows, metadata, cog_category)
 
-metadata.to_csv("Malaspina-genes-coverage-OM-RGC.csv", index=False)
+	metadata.to_csv("Malaspina-genes-coverage-OM-RGC.csv", index=False)
 
 if __name__ == "__main__":
 	main()
